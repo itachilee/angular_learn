8b153049f569d3507316e56981085b58249cbe2d
itachilee <3050155685@qq.com>
2020-07-15 14:38:20 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { enableProdMode } from '@angular/core';
+// 连同Angular编译器一起发布到浏览器
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -7,6 +8,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//Angular编译器在浏览器中编译并引导该应用
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
